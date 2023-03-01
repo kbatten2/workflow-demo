4f816987726c4b562c391ce6a9144ebb185c3e78
Keiran Batten <kbatten@deloitte.co.nz>
2023-03-02 07:43:20 +1300

    added values for testing

--- src/main.tf
+++ src/main.tf
@@ -1,2 +1,9 @@
-# comment
-# check trigger 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.13.0"
+    }
+  }
+  required_version = ">= 1.2.4"
+}
